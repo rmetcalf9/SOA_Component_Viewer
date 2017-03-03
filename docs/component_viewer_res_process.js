d8b09b17485e7e49d31e80b52b1b29b386c98614
Robert Metcalf <rmetcalf9@googlemail.com>
2017-03-03 08:59:16 +0000

    Only write back orders if user has access

--- docs/component_viewer_res_process.js
+++ docs/component_viewer_res_process.js
@@ -23,22 +23,24 @@ function component_viewer_res_process_ScheduleResourses() {
 	};
 	*/
 	
-	//console.log("If sort order results in changes write new order to googlesheet");
-	//This step will fill any any items with missing orders and spread out where mutiple items have same order
-	board_prepare_saveBatch();
-	var cur_rec = 0;
-	var num_changed = 0;
-	for (var cur_do = 0; cur_do < dataObjects.RESOURCEALLOCATIONkeys.length; cur_do++) {
-		cur_rec += 10;
-		var res_alloc_obj = dataObjects.RESOURCEALLOCATIONs[dataObjects.RESOURCEALLOCATIONkeys[cur_do]];
-		if (res_alloc_obj.binpackpriority != cur_rec) {
-			res_alloc_obj.binpackpriority = cur_rec;
-			component_viewer_res_data_save_resourse_allocation_into_batch(res_alloc_obj.uid)
-			num_changed ++;
+	if (accessLevel=="READWRITE") {
+		//console.log("If sort order results in changes write new order to googlesheet");
+		//This step will fill any any items with missing orders and spread out where mutiple items have same order
+		board_prepare_saveBatch();
+		var cur_rec = 0;
+		var num_changed = 0;
+		for (var cur_do = 0; cur_do < dataObjects.RESOURCEALLOCATIONkeys.length; cur_do++) {
+			cur_rec += 10;
+			var res_alloc_obj = dataObjects.RESOURCEALLOCATIONs[dataObjects.RESOURCEALLOCATIONkeys[cur_do]];
+			if (res_alloc_obj.binpackpriority != cur_rec) {
+				res_alloc_obj.binpackpriority = cur_rec;
+				component_viewer_res_data_save_resourse_allocation_into_batch(res_alloc_obj.uid)
+				num_changed ++;
+			};
+		}
+		if (num_changed>0) {
+			board_execute_saveBatch(spreadsheetId);
 		};
-	}
-	if (num_changed>0) {
-		board_execute_saveBatch(spreadsheetId);
 	};
 
 	//console.log("TODO Clear all current scheduled allocations");
